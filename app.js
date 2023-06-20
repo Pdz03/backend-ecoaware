dec57b1ce97b9d3dfc43451daed108953ceffa67
Syaifudin Fendi Prasetyo <syaifudinfendip@gmail.com>
2023-06-20 17:58:33 +0700

    origin fix

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(session({
   store: sessionStore,
   cookie: {
     // sameSite: 'none',
-    secure: false, // ubah menjadi true jika menggunakan HTTPS
+    secure: true, // ubah menjadi true jika menggunakan HTTPS
     // httpOnly: true,
     maxAge: 24 * 60 * 60 * 1000, // contoh: sesi berlaku selama 1 hari (dalam milidetik)
   },
@@ -31,7 +31,7 @@ app.use(session({
 app.use(express.static('src/uploads'));
 
 app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*'); // Ganti dengan domain front-end yang digunakan
+  res.setHeader('Access-Control-Allow-Origin', 'https://ecoaware-apps.netlify.app/'); // Ganti dengan domain front-end yang digunakan
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true'); // Jika menggunakan kredensial (misalnya: cookie)
